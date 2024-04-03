2bd0233e8c6cb67f45754681d70d26c1be0637cd
piotr-lewandowski <lewandowskipiotr98@gmail.com>
2024-04-03 19:45:22 +0200

    changed print to file write

--- gen_data.py
+++ gen_data.py
@@ -3,18 +3,22 @@ from random import Random
 
 # c is a polynomial, z and y are 4x1 matrices of polynomials
 # prints the coefficients as 256-element arrays
-def print_results(c, z, y, s1):
-    print("c=")
-    print(c.copy_from_ntt().coeffs)
-    print("z=")
+def print_results(c, z, y, s1, f):
+    f.write("c=\n")
+    f.write(str(c.copy_from_ntt().coeffs))
+    f.write("\n")
+    f.write("z=\n")
     for row in z:
-        print(row[0].coeffs)
-    print("y=")
+        f.write(str(row[0].coeffs))
+        f.write("\n")
+    f.write("y=\n")
     for row in y:
-        print(row[0].coeffs)
-    print("s1=")
+        f.write(str(row[0].coeffs))
+        f.write("\n")
+    f.write("s1=\n")
     for row in s1:
-        print(row[0].coeffs)
+        f.write(str(row[0].coeffs))
+        f.write("\n")
 
 
 if __name__ == "__main__":
@@ -22,11 +26,13 @@ if __name__ == "__main__":
     dilithium = Dilithium2
     pk, sk = dilithium.keygen()
 
-    rounds = 10
+    rounds = 100
     for i in range(rounds):
         msg = random.randbytes(32)
         sig = dilithium.sign(sk, msg)
     
     results = dilithium.leaked_data
+    f = open("output", "w")
     for (c, z, y, s1) in results:
-        print_results(c, z, y, s1)
+        print_results(c, z, y, s1, f)
+    f.close()
