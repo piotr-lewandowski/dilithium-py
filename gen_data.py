b527723d853896cd8c99d29424c7f646c4f6bd91
Oskar Werner <nsz.werneros@gmail.com>
2024-04-04 08:45:07 +0200

    filter y with 0

--- gen_data.py
+++ gen_data.py
@@ -3,6 +3,8 @@ from random import Random
 
 # c is a polynomial, z and y are 4x1 matrices of polynomials
 # prints the coefficients as 256-element arrays
+
+
 def print_results(c, z, y, s1, f):
     f.write("c=\n")
     f.write(str(c.copy_from_ntt().coeffs))
@@ -26,13 +28,16 @@ if __name__ == "__main__":
     dilithium = Dilithium2
     pk, sk = dilithium.keygen()
 
-    rounds = 100
+    rounds = 100000
     for i in range(rounds):
+        if i % 1000 == 0:
+            print(i // 1000)
         msg = random.randbytes(32)
         sig = dilithium.sign(sk, msg)
-    
+
     results = dilithium.leaked_data
-    f = open("output", "w")
+    f = open("output.txt", "w")
     for (c, z, y, s1) in results:
-        print_results(c, z, y, s1, f)
+        if any(list(map((lambda x: x == 0), y[0][0].coeffs))):
+            print_results(c, z, y, s1, f)
     f.close()
