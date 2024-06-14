618f78ae06d405f5b18c1c0c71c8df892ed0ef37
piotr-lewandowski <lewandowskipiotr98@gmail.com>
2024-06-14 14:39:12 +0200

    Added csv -> ampl conversion

---
+++ csv_to_ampl/ampl_test.py
@@ -0,0 +1,43 @@
+
+from numpy.polynomial.polynomial import polyadd, polymul
+from amplpy import AMPL, ampl_notebook
+from csv_data import z, L, C, s1ij
+# https://amplpy.ampl.com/en/latest/?_gl=1*13rarp*_ga*NTY1NTY0OTM2LjE2OTcwNDg0MjI.*_ga_FY84K2YRRE*MTcxNDg5OTEyNy41LjEuMTcxNDg5OTkxMi4wLjAuMA..
+ampl = AMPL() # instantiate AMPL object
+
+ampl.eval(
+    """
+set POLYNOMIAL_LENGTH = 0..255;
+set L dimen 2; # (m, j) z danych
+
+param z {L};  # dla pary (m,j) z danych zij
+param s1 {L};  # dla pary (m,j) z danych s1ij
+
+param C { L , POLYNOMIAL_LENGTH}; 
+param K = 1000000;
+
+var X {L} binary; 
+var s {POLYNOMIAL_LENGTH} integer; 
+
+maximize number_of_coeffs: sum {(m,j) in L} X[m,j]; 
+
+subject to res_1 {(m,j) in L}: 
+	z[m, j] - sum {i in POLYNOMIAL_LENGTH: i <= j} C[m,j, i]*s[j - i]
+		+ sum {i in POLYNOMIAL_LENGTH: i > j} C[m,j, i]*s[256 + j - i] <= K*(1 - X[m,j]); 
+		
+subject to res_2 {(m,j) in L}: 
+	z[m,j] - sum {i in POLYNOMIAL_LENGTH: i <= j} C[m,j,i]*s[j - i]
+		+ sum {i in POLYNOMIAL_LENGTH: i > j} C[m,j, i]*s[256 + j - i] >= -K*(1 - X[m,j]); 
+ """
+)
+
+
+ampl.getSet('L').set_values(L)
+# ampl.getParameter('s').setValues(z)
+ampl.getParameter('z').setValues(z)
+ampl.getParameter('s1').setValues(s1ij)
+
+ampl.getParameter('C').setValues(C)
+# ampl.solve(solver="cplex")
+ampl.export_data("dil_i0_new_old.dat")
+ampl.export_model("dil.mod")
---
+++ csv_to_ampl/csv_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+df = pd.read_csv("final.csv")
+df["c"] = (
+    df
+    .filter(regex=r"c\d+")
+    .apply(lambda r: r.dropna().to_list(), result_type="reduce", axis=1)
+    )
+
+df = df.drop(df.filter(regex=r"c\d+").columns, axis=1)
+
+
+my_df = df[df["i"] == 0]
+Ldf = my_df[['m', 'j']]
+L = Ldf.values.tolist()
+L = [ tuple(p) for p in L]
+
+Z =  my_df['zij'].values.tolist()
+
+x = list(zip(L,Z))
+z = {tuple(k): v for k, v in x}
+
+
+Ccol =  my_df['c'].values.tolist()
+
+czip = list(zip(L,Ccol))
+C = {}
+for ci in czip:        
+        C = C | {tuple([ci[0][0],ci[0][1], j ] ): ci[1][j]  for j in range(256)}
+
+x = list(zip(L,Z))
+S1ij = my_df["s1ij"].values.tolist()
+x = list(zip(L,S1ij))
+s1ij = {tuple(k): v for k, v in x}
+
