8ab39b5abbcb1429b46bc59eb00ac4473e9f18d0
piotr-lewandowski <lewandowskipiotr98@gmail.com>
2024-08-09 14:08:42 +0200

    updated AMPL data converter

--- csv_to_ampl/csv_data.py
+++ csv_to_ampl/csv_data.py
@@ -1,37 +1,35 @@
 import pandas as pd
 
-file = "non_ntt_filtered"
+def csv_to_ampl(file):
+    df = pd.read_csv(f"./data/{file}.csv")
+    df["c"] = (
+        df
+        .filter(regex=r"c\d+")
+        .apply(lambda r: r.dropna().to_list(), result_type="reduce", axis=1)
+        )
 
-df = pd.read_csv(f"../data/{file}.csv")
-df["c"] = (
-    df
-    .filter(regex=r"c\d+")
-    .apply(lambda r: r.dropna().to_list(), result_type="reduce", axis=1)
-    )
+    df = df.drop(df.filter(regex=r"c\d+").columns, axis=1)
 
-df = df.drop(df.filter(regex=r"c\d+").columns, axis=1)
+    my_df = df[df["i"] == 0]
+    Ldf = my_df[['m', 'j']]
+    L = Ldf.values.tolist()
+    L = [ tuple(p) for p in L]
 
+    Z =  my_df['zij'].values.tolist()
 
-my_df = df[df["i"] == 0]
-Ldf = my_df[['m', 'j']]
-L = Ldf.values.tolist()
-L = [ tuple(p) for p in L]
+    x = list(zip(L,Z))
+    z = {tuple(k): v for k, v in x}
 
-Z =  my_df['zij'].values.tolist()
+    Ccol =  my_df['c'].values.tolist()
 
-x = list(zip(L,Z))
-z = {tuple(k): v for k, v in x}
+    czip = list(zip(L,Ccol))
+    C = {}
+    for ci in czip:        
+            C = C | {tuple([ci[0][0],ci[0][1], j ] ): ci[1][j]  for j in range(256)}
 
-
-Ccol =  my_df['c'].values.tolist()
-
-czip = list(zip(L,Ccol))
-C = {}
-for ci in czip:        
-        C = C | {tuple([ci[0][0],ci[0][1], j ] ): ci[1][j]  for j in range(256)}
-
-x = list(zip(L,Z))
-S1ij = my_df["s1ij"].values.tolist()
-x = list(zip(L,S1ij))
-s1ij = {tuple(k): v for k, v in x}
-
+    x = list(zip(L,Z))
+    S1ij = my_df["s1ij"].values.tolist()
+    x = list(zip(L,S1ij))
+    s1ij = {tuple(k): v for k, v in x}
+    
+    return z, L, C, s1ij
--- csv_to_ampl/generate_ampl.py
+++ csv_to_ampl/generate_ampl.py
@@ -1,39 +1,32 @@
 from numpy.polynomial.polynomial import polyadd, polymul
 from amplpy import AMPL
-from csv_data import z, L, C, s1ij
+from csv_data import csv_to_ampl
+from sys import argv
+
 ampl = AMPL()
 
 ampl.eval(
-    """
+"""
 set POLYNOMIAL_LENGTH = 0..255;
-set L dimen 2; # (m, j) z danych
-
-param z {L};  # dla pary (m,j) z danych zij
-param s1 {L};  # dla pary (m,j) z danych s1ij
+set L dimen 2;
+param z {L};
+param s1 {L};
 
 param C { L , POLYNOMIAL_LENGTH}; 
 param K = 1000000;
-
-var X {L} binary; 
-var s {POLYNOMIAL_LENGTH} integer; 
-
-maximize number_of_coeffs: sum {(m,j) in L} X[m,j]; 
-
-subject to res_1 {(m,j) in L}: 
-	z[m, j] - sum {i in POLYNOMIAL_LENGTH: i <= j} C[m,j, i]*s[j - i]
-		+ sum {i in POLYNOMIAL_LENGTH: i > j} C[m,j, i]*s[256 + j - i] <= K*(1 - X[m,j]); 
-		
-subject to res_2 {(m,j) in L}: 
-	z[m,j] - sum {i in POLYNOMIAL_LENGTH: i <= j} C[m,j,i]*s[j - i]
-		+ sum {i in POLYNOMIAL_LENGTH: i > j} C[m,j, i]*s[256 + j - i] >= -K*(1 - X[m,j]); 
- """
+"""
 )
 
+def generate_ampl(dataset: str):
+	z, L, C, s1ij = csv_to_ampl(dataset)
 
-ampl.getSet('L').set_values(L)
-ampl.getParameter('z').setValues(z)
-ampl.getParameter('s1').setValues(s1ij)
+	ampl.getSet('L').set_values(L)
+	ampl.getParameter('z').setValues(z)
+	ampl.getParameter('s1').setValues(s1ij)
 
-ampl.getParameter('C').setValues(C)
-ampl.export_data("dil.dat")
-ampl.export_model("dil.mod")
+	ampl.getParameter('C').setValues(C)
+	ampl.export_data(f"./csv_to_ampl/{dataset}.dat")
+
+if __name__ == "__main__":
+	file = argv[1]
+	generate_ampl(file)
